1a65a9105bde5703e56949bb4322aa6c1ec279f2
Eleni <57090719+eleniaslanidou89@users.noreply.github.com>
2021-08-24 12:50:06 +0200

    Products-Toolbar
    added style for Products component and a toolbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,13 +2,30 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import Product from './Product/Product'
+import useStyles from './styles'
 const products = [
-    { id:1, name:'Shoes', description:'Running shoes', price:'$150', image:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'},
-    { id:2, name:'High heels', description:'High heels', price:'$200', image:'https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80'}
+  {
+    id: 1,
+    name: 'Shoes',
+    description: 'Running shoes',
+    price: '$150',
+    image:
+      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
+  },
+  {
+    id: 2,
+    name: 'High heels',
+    description: 'High heels',
+    price: '$200',
+    image:
+      'https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80',
+  },
 ]
 const Products = () => {
+  const classes = useStyles()
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
